d6a96e1ffef3e43b87f6a52f361538ac3fe0d95a
SigsSubo <subo.sigdel@gmail.com>
2025-04-17 09:23:35 +1000

    added logger to BillingGrpcService

--- billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
+++ billing-service/src/main/java/com/pm/billingservice/grpc/BillingGrpcService.java
@@ -2,14 +2,20 @@ package com.pm.billingservice.grpc;
 
 
 import billing.BillingServiceGrpc.BillingServiceImplBase;
+// StreamObserver is used by both the client stubs and service implementations for sending or receiving stream messages
+// https://grpc.github.io/grpc-java/javadoc/io/grpc/stub/StreamObserver.html
 import io.grpc.stub.StreamObserver;
 import net.devh.boot.grpc.server.service.GrpcService;
+import org.slf4j.LoggerFactory;
 
 @GrpcService
 public class BillingGrpcService extends BillingServiceImplBase {
+    private static final Logger log = LoggerFactory.getLogger(BillingGrpcService.class);
     @Override
     public void createBillingAccount(billing.BillingRequest billingRequest,
             StreamObserver<billing.BillingResponse> responseObserver) {
 
+        log.info("createBillingAccount request received {}", billingRequest.toString());
+        // Business Login - e.g save to database, perform calculations etc
     }
 }
