dc4e9ed49e23acb06e042d47cf29122c66d75a29
SigsSubo <subo.sigdel@gmail.com>
2025-04-19 16:39:01 +1000

    removed bind ports on auth-service docker container, to be only accessed via api-gateway. Check login.http for info

--- auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
+++ auth-service/src/main/java/com/pm/authservice/util/JwtUtil.java
@@ -1,5 +1,6 @@
 package com.pm.authservice.util;
 
+import io.jsonwebtoken.JwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -7,9 +8,11 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
+import javax.crypto.SecretKey;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
+import java.security.SignatureException;
 import java.util.Base64;
 import java.util.Date;
 
@@ -33,5 +36,15 @@ public class JwtUtil {
                 .signWith(secretKey)
                 .compact();
 
+    }
+
+    public void validateToken(String token) {
+        try {
+            Jwts.parser().verifyWith((SecretKey) secretKey)
+                    .build()
+                    .parseSignedClaims(token);
+        } catch (JwtException e) {
+            throw new JwtException("Invalid JWT");
+        }
     }
 }
