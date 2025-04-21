bc6fdc4c5b5c0566ba3541d3975a78cbd8ce17cd
SigsSubo <subo.sigdel@gmail.com>
2025-04-21 16:47:54 +1000

    configuring local stack

--- integration-tests/src/test/java/PatientIntegrationTest.java
+++ integration-tests/src/test/java/PatientIntegrationTest.java
@@ -1,7 +1,6 @@
 import io.restassured.RestAssured;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
-import io.restassured.response.Response;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.notNullValue;
 
